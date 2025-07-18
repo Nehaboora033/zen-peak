c3bf5a19ab389924a5b9fbf7e8e47816052f8c47
Neha <nehaboora49@gmail.com>
2025-07-18 09:22:02 +0530

    added hover on button

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -6,15 +6,17 @@ import Button from './common/Button'
 
 const Herosection = () => {
   return (
-    <div style={{ backgroundImage: `url(${backgroundImage})` }} className='pt-[99px] bg-no-repeat bg-cover bg-center ' >
-      <div className='max-w-[1140px] px-3 mx-auto pb-[396px] sm:pb-[600px]'>
-        <img src={logo} alt="image" className='mx-auto  sm:w-[480px] w-[240px] h-[50%]' />
-        <SubHeading className='text-[white] max-w-[1032px] text-center mt-[28px] mx-auto' StartItalicText={'Exploring'} EndItalicText={'Market Strategies'} BoldText={'Crypto and Primary'} />
-        <Button className='rounded-[112px] text-xl px-[46px] py-[20px] sm:py-[25px] w-full text-center sm:max-w-[202px] sm:mx-auto mt-[40px]' ButtonText={'Contact Us'}/>
-        <div>
+   
+      <div style={{ backgroundImage: `url(${backgroundImage})` }} className='pt-[99px] bg-no-repeat bg-cover bg-center ' >
+        <div className='max-w-[1140px] px-3 mx-auto pb-[396px] sm:pb-[600px]'>
+          <img src={logo} alt="image" className='mx-auto  sm:w-[480px] w-[240px] h-[50%]' />
+          <SubHeading className='text-[white] max-w-[1032px] text-center mt-[28px] mx-auto' StartItalicText={'Exploring'} EndItalicText={'Market Strategies'} BoldText={'Crypto and Primary'} />
+          <Button className='rounded-[112px] text-xl px-[46px] py-[20px] sm:py-[25px] w-full text-center sm:max-w-[202px] sm:mx-auto mt-[40px]' ButtonText={'Contact Us'}/>
+          <div>
+          </div>
         </div>
       </div>
-    </div>
+   
   )
 }
 
--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ButtonText,className=''}) => {
   return (
-    <div className={`${className} text-white font-medium border border-white `}>{ButtonText} </div>
+    <div className={`${className} text-white font-medium border border-white duration-200 cursor-pointer hover:scale-105 transition-transform active:scale-105 `}>{ButtonText} </div>
   )
 }
 
